1426086cc59045dfc487ddc3ef5da8c08a58ae38
Ben Kuehnert <bkuehnert@gmail.com>
2020-02-27 14:52:41 -0500

    added flatten macro

--- macro.lisp
+++ macro.lisp
@@ -2,3 +2,11 @@
 (defmacro alambda (parms &body body)
   `(labels ((self ,parms ,@body))
      #'self))
+
+;; Graham's flatten
+(defun flatten (x)
+  (labels ((rec (x acc)
+             (cond ((null x) acc)
+                   ((atom x) (cons x acc))
+                   (t (rec (car x) (rec (cdr x) acc))))))
+    (rec x nil)))
