37ed488023630657073edd3f17e1a3c36beb5d9e
Ben Kuehnert <bkuehnert@gmail.com>
2020-05-20 14:32:57 -0400

    update to using new link object, functions up to assert-before are updated.

--- timepoint.lisp
+++ timepoint.lisp
@@ -3,102 +3,148 @@
 ;;; * chain: Unique chain identifier. Two timepoints will have the same
 ;;;   chain value iff they are on the same chain.
 ;;;
-;;; * prev: Previous timepoint in the chain.
+;;; * prev: Link object corresponding to the previous link in the chain.
 ;;;
-;;; * next: Next timepoint in the chain.
+;;; * next: Link object corresponding to the next link in the chain.
 ;;;
-;;; * ptime: Pseudotime of a timepoint
+;;; * ptime: Pseudotime of a timepoint.
 ;;;
-;;; * inc: List of timepoints that have cross chain links into the
-;;;   timepoint.
+;;; * inc: List of cross-chain links entering the timepoint.
 ;;;
-;;; * out: List of timepoints that have cross chain links out of the
-;;;   timepoint.
+;;; * out: List of cross-chain links exiting the timepoint.
 ;;;
 ;;; * upper: Numerical absolute upper bound on the timepoint.
 ;;;
 ;;; * lower: Numerical absolute lower bound on the timepoint.
 ;;;
-;;; * refs: List of episode names which point to the timepoint.
+;;; * brefs: List of episode names which the timepoint begins.
+;;; 
+;;; * erefs: List of episode names which the timepoint ends.
 
 (defclass timepoint ()
   ((chain :initarg :chain
-		  :accessor tp-chain)
+	  :accessor tp-chain)
+
    (prev :initarg :prev
-		 :accessor tp-prev)
+	 :accessor tp-prev)
+
    (next :initarg :next
-		 :accessor tp-next)
+	 :accessor tp-next)
+
    (ptime :initarg :ptime
-		  :accessor tp-ptime)
+	  :accessor tp-ptime)
+
+   (upper :initarg :upper
+	  :accessor tp-upper)
+
+   (lower :initarg :lower
+	  :accessor tp-lower)
+
    (inc :initarg :inc
-		:accessor tp-inc)
+	:accessor tp-inc)
+
    (out :initarg :out
-		:accessor tp-out)
+	:accessor tp-out)
+
    (brefs :initarg :brefs
-		  :accessor tp-brefs)
+	  :accessor tp-brefs)
+
    (erefs :initarg :erefs
-		  :accessor tp-erefs)))
+	  :accessor tp-erefs)))
 
 (defun make-timepoint (&key (chain (sxhash (gensym))) 
-							prev next (ptime 1) in out ;upper lower 
-							brefs erefs)
+			 prev next (ptime 1) in out upper lower brefs erefs)
   (make-instance 'timepoint
-				 :chain chain
-				 :prev prev
-				 :next next
-				 :ptime ptime
-				 :inc in
-				 :out out
-				 ;:upper upper
-				 ;:lower lower
-				 :brefs brefs
-				 :erefs erefs))
+		 :chain chain
+		 :prev prev
+		 :next next
+		 :ptime ptime
+		 :inc in
+		 :out out
+		 :upper upper
+		 :lower lower
+		 :brefs brefs
+		 :erefs erefs))
+
+
+(defclass link ()
+  ((chain :initarg :chain
+	  :accessor link-chain)
+   (src :initarc :src
+	:accessor link-src)
+   (dst :initarg :dst
+	:accessor link-dst)
+   (lower :initarg :lower
+	  :accessor link-lower)
+   (upper :initarg :upper
+	  :accessor link-upper)))
+
+(defun make-link (&key chain src dst lower upper)
+  (make-instance 'link
+		 :chain chain
+		 :src src
+		 :dst dst
+		 :lower lower
+		 :upper upper))
 
 ;;; Utility Functions
 ;;; -----------------------------------------------------------------------
 
 ;;; Get list of direct successors of a timepoint t1
 (defun get-successors (t1)
-  (cond ((not t1) nil)
-		((not (tp-next t1)) (tp-out t1))
-		(t (cons (tp-next t1) (tp-out t1)))))
+  (when t1
+    (let ((succ (mapcar (lambda (x) (link-dst x)) (tp-out t1))))
+      (if (tp-next t1)
+	  (cons (link-dst (tp-next t1)) succ)
+	  succ))))
 
 ;;; Get list of direct ancestors of a timepoint t1
 (defun get-ancestors (t1)
-  (cond ((not t1) nil)
-		((not (tp-prev t1)) (tp-inc t1))
-		(t (cons (tp-prev t1) (tp-inc t1)))))
+  (when t1
+    (let ((anc (mapcar (lambda (x) (link-src x)) (tp-inc t1))))
+      (if (tp-prev t1)
+	  (cons (link-src (tp-prev t1)) anc)
+	  anc))))
 
 ;;; Get list of all successors of a timepoint t1
 (defun get-all-successors (t1)
   (flatten
-	(funcall 
-	  (alambda (t1 seen)
-	    (cond ((not (gethash t1 seen))
-			   (setf (gethash t1 seen) t)
-			   (cons t1 (mapcar (lambda (tk) 
-								  (self tk seen)) (get-successors t1))))))
-		t1 (make-hash-table :test #'equal))))
+   (funcall 
+    (alambda (t1 seen)
+	     (cond ((not (gethash t1 seen))
+		    (setf (gethash t1 seen) t)
+		    (cons t1 (mapcar (lambda (tk) 
+				       (self tk seen))
+				     (get-successors t1))))))
+    t1 (make-hash-table :test #'equal))))
 
-;;; Get list of all ancestors of a timepoint t1
+;;; Get list of all ancestors of a timepoint t1.
 (defun get-all-ancestors (t1)
   (flatten
-	(funcall 
-	  (alambda (t1 seen)
-	    (cond 
-		  ((not (gethash t1 seen))
-	       (setf (gethash t1 seen) t)
-	       (cons t1 (mapcar (lambda (tk) 
-							  (self tk seen)) (get-ancestors t1))))))
-		t1 (make-hash-table :test #'equal))))
+   (funcall 
+    (alambda (t1 seen)
+	     (cond 
+	       ((not (gethash t1 seen))
+		(setf (gethash t1 seen) t)
+		(cons t1 (mapcar (lambda (tk) 
+				   (self tk seen))
+				 (get-ancestors t1))))))
+    t1 (make-hash-table :test #'equal))))
 
-;;; Check if a timepoint t1 is the last in its chain
+;;; Check if a timepoint t1 is the last in its chain.
 (defun last-p (t1)
   (not (tp-next t1)))
 
-;;; Check if a timepoint t1 is the first in its chain
+;;; Check if a timepoint t1 is the first in its chain.
 (defun first-p (t1)
   (not (tp-prev t1)))
+
+;;; For two existing timepoints t1 and t2, establish a cross-chain link t1 -> t2.
+(defun add-cross-link (t1 t2 )
+  (when (and t1 t2)
+    (let ((newlink (make-link :src t1 :dst t2)))
+      (push newlink (tp-out t1))
+      (push newlink (tp-inc t2)))))
 
 ;;; Insertion methods
 ;;; -----------------------------------------------------------------------
@@ -107,46 +153,51 @@
 ;;; directly after t1 in the graph.
 (defun insert-timepoint-after (t1 &key brefs erefs)
   (let ((ret (gensym)))
-	(cond 
-	  ((not t1) nil) ;;; add error message here
-	  ((last-p t1)
-	   (setf ret (make-timepoint
-				   :chain (tp-chain t1)
-				   :prev t1
-				   :ptime (1+ (tp-ptime t1))
-				   :brefs brefs
-				   :erefs erefs))
-	   (setf (tp-next t1) ret)
-	   ret)
-	  (t
-		(setf ret (make-timepoint 
-					:in (list t1) 
-					:brefs brefs
-					:erefs erefs))
-		(setf (tp-out t1) (cons ret (tp-out t1)))
-		ret))))
+    (cond 
+      ((not t1) nil) ;;; add error message here
+
+      ((last-p t1)
+       (setf ret (make-timepoint
+		  :chain (tp-chain t1)
+		  :ptime (1+ (tp-ptime t1))
+		  :lower (tp-lower t1)
+		  :brefs brefs
+		  :erefs erefs))
+       (let ((newlink (make-link :chain (tp-chain t1) :src t1 :dst ret)))
+	 (setf (tp-prev ret) newlink)
+	 (setf (tp-next t1) newlink))
+
+       ret)
+
+      (t
+       (setf ret (make-timepoint :brefs brefs :erefs erefs))
+       (add-cross-link t1 ret)
+       ret))))
 
 ;;; Given a timepoint t1, creates and returns a new timepoint which is
 ;;; directly before t1 in the graph.
 (defun insert-timepoint-before (t1 &key brefs erefs)
   (let ((ret (gensym)))
-	(cond 
-	  ((first-p t1)
-	   (setf ret (make-timepoint
-				   :chain (tp-chain t1)
-				   :next t1
-				   :ptime (1- (tp-ptime t1))
-				   :brefs brefs
-				   :erefs erefs))
-	   (setf (tp-prev t1) ret)
-	   ret)
-	  (t
-		(setf ret (make-timepoint 
-					:out (list t1) 
-					:brefs brefs
-					:erefs erefs))
-		(setf (tp-inc t1) (cons ret (tp-inc t1)))
-		ret))))
+    (cond 
+      ((not t1) nil) ; add error
+
+      ((first-p t1)
+       (setf ret (make-timepoint
+		  :chain (tp-chain t1)
+		  :ptime (1- (tp-ptime t1))
+		  :upper (tp-upper t1)
+		  :brefs brefs
+		  :erefs erefs))
+       (let ((newlink (make-link :chain (tp-chain t1) :src ret :dst t1)))
+	 (setf (tp-next ret) newlink)
+	 (setf (tp-prev t1) newlink))
+
+       ret)
+	 
+      (t
+       (setf ret (make-timepoint :brefs brefs :erefs erefs))
+       (add-cross-link ret t1)
+       ret))))
 
 ;;; t1 and t2 are timepoints (either are possibly nil), assert that t1 is
 ;;; before t2 and return them. Preconditions: If t1 and t2 are not nil,
@@ -155,36 +206,36 @@
 ;;; function.
 (defun tp-assert-before (t1 t2)
   (cond
+    ((and (not t1) (not t2))
+     (let* ((t1 (make-timepoint))
+	    (t2 (insert-timepoint-after t1)))
+       (list t1 t2)))
 
-	((and (not t1) (not t2))
-	 (let* ((t1 (make-timepoint))
-			(t2 (insert-timepoint-after t1)))
-	   (list t1 t2)))
+    ((not t1)
+     (list (insert-timepoint-before t2) t2))
 
-	((not t1)
-	 (list (insert-timepoint-before t2) t2))
+    ((not t2)
+     (list t1 (insert-timepoint-after t1)))
 
-	((not t2)
-	 (list t1 (insert-timepoint-after t1)))
-
-	((tp-before-p t1 t2)
-	 (list t1 t2))
+    ((tp-before-p t1 t2)
+     (list t1 t2))
 
     ((and (last-p t1) (first-p t2))
-     (setf (tp-next t1) t2)
-     (setf (tp-prev t2) t1)
-     (funcall (alambda (tk ptime hash)
-		(when tk
-		  (setf (tp-ptime tk) (+ ptime (tp-ptime tk)))
-		  (setf (tp-chain tk) hash)
-		  (self (tp-next tk) ptime hash)))
+     (let ((newlink (make-link :chain (tp-chain t1) :src t1 :dst t2)))
+       (setf (tp-next t1) newlink)
+       (setf (tp-prev t2) newlink))
+     (funcall
+      (alambda (tk ptime hash)
+	(when tk
+	  (setf (tp-ptime tk) (+ ptime (tp-ptime tk)))
+	  (setf (tp-chain tk) hash)
+	  (self (tp-next tk) ptime hash)))
       t2 (tp-ptime t1) (tp-chain t1))
-	 (list t1 t2))
+     (list t1 t2))
 
     (t
-     (push t2 (tp-out t1))
-     (push t1 (tp-inc t2))
-	 (list t1 t2))))
+     (add-cross-link t1 t2)
+     (list t1 t2))))
 
 ;;; t1 and t2 are timepoints (either are possibly nil), assert that t1 is
 ;;; equal to t2 and return (t1 t2). Preconditions: none. Function also
@@ -225,15 +276,15 @@
 ;;; timegraph object tg.
 (defun tp-assert-equal-helper (tg t1 t2)
   (let* ((t1suc (get-all-successors t1))
-		 (t2anc (get-all-ancestors t2))
-		 (quo (remove t1 (intersection t1suc t2anc))))
-	(when (member (tp-next t1) quo) 
-	  (setf (tp-next t1) nil))
+	 (t2anc (get-all-ancestors t2))
+	 (quo (remove t1 (intersection t1suc t2anc))))
 
-	;(inspect quo)
+    (when (member (tp-next t1) quo) 
+      (setf (tp-next t1) nil))
 
-	(setf (tp-inc t1) (remove-if (lambda (x) (member x quo)) (tp-inc t1)))
-	(setf (tp-out t1) (remove-if (lambda (x) (member x quo)) (tp-out t1)))
+    (setf (tp-inc t1) (remove-if (lambda (x) (member x quo)) (tp-inc t1)))
+
+    (setf (tp-out t1) (remove-if (lambda (x) (member x quo)) (tp-out t1)))
 
 	(dolist (tk quo)
 	  (let ((tk-inc (remove-if (lambda (x) (member x (cons t1 quo)))
